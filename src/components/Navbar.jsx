541e72e264a4ed07df1afa5de2bc85ee012ee897
mohammad <mohammadpirasteh1382@gmail.com>
2023-05-13 00:35:36 -0700

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import '../Style/main.css'
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+        <div>
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+        </div>
+	);
+}
+
+export default Navbar;
